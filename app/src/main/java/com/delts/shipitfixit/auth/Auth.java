fc1e28d6253b9b5faeb74914d27e5476decd3e0b
Delthoid <delthonvillanueva20@gmail.com>
2023-04-30 15:24:19 +0800

    Added Auth class

--- app/src/main/java/com/delts/shipitfixit/auth/Auth.java
+++ app/src/main/java/com/delts/shipitfixit/auth/Auth.java
@@ -1,5 +1,36 @@
 package com.delts.shipitfixit.auth;
 
-public class Auth {
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import com.delts.shipitfixit.models.User;
+
+public class Auth {
+    private final Context context;
+
+    public Auth(Context context) {
+        this.context = context;
+    }
+
+    private static final String preferenceFileKey = "auth";
+
+    //Login
+    public void login(User user) {
+        SharedPreferences sharedPref = context.getSharedPreferences(preferenceFileKey, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+    }
+
+    //Logout
+    public void logout() {
+
+    }
+
+    public void register(Context context) {
+
+    }
+
+    //Check if there is saved user id
+    public void checkLoggedUsers() {
+
+    }
 }
