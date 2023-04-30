0825b34c532a545d5aab5f77f17d7d080d46900c
Jacob Del Rosario <jovenjacobdelrosario14@gmail.com>
2023-04-30 17:58:43 +0800

    UserDBHelper

--- app/src/main/java/com/delts/shipitfixit/database/UsersDatabaseHelper.java
+++ app/src/main/java/com/delts/shipitfixit/database/UsersDatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.delts.shipitfixit.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,9 +15,9 @@ public class UsersDatabaseHelper extends SQLiteOpenHelper {
     public static final int DB_VERSION = 1;
 
     //Queries
-    private static final String SQL_CREATE_DB = "" +
+    private static final String SQL_CREATE_DB =
             "CREATE TABLE " + Entries.UserEntry.TABLE_NAME + "(" +
-            Entries.UserEntry._ID + " INTEGER PRIMARY KEY, " +
+            Entries.UserEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             Entries.UserEntry.COLUMN_NAME_USERNAME + " TEXT," +
             Entries.UserEntry.COLUMN_NAME_PASSWORD + " TEXT)";
 
@@ -25,11 +27,54 @@ public class UsersDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        db.execSQL();
+        db.execSQL(SQL_CREATE_DB);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String onUpgrade = "DROP TABLE IF EXISTS " + Entries.UserEntry.TABLE_NAME;
+        db.execSQL(onUpgrade);
+    }
 
+    //returns true if is already existing
+    public boolean checkUsernameIfExist(String username){
+        String QUERY = "SELECT " + Entries.UserEntry.COLUMN_NAME_USERNAME + " WHERE "
+            + Entries.UserEntry.COLUMN_NAME_USERNAME + " = ?";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(QUERY, new String[] {username});
+
+        db.close();
+        cursor.close();
+        return cursor.moveToNext() ? true : false;
+    }
+
+    //inserting account here return false if failed
+    public boolean insertAccount(String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(Entries.UserEntry.COLUMN_NAME_USERNAME, username);
+        cv.put(Entries.UserEntry.COLUMN_NAME_PASSWORD, password);
+        long result = db.insert(Entries.UserEntry.TABLE_NAME, null, cv);
+
+        boolean success = result != -1 ? true : false;
+        db.close();
+        return success;
+    }
+
+    //returns true if username and password matches together
+    public boolean checkLoginSuccess(String username, String password){
+        String QUERY = "SELECT " + Entries.UserEntry.COLUMN_NAME_USERNAME + ", "
+                + Entries.UserEntry.COLUMN_NAME_PASSWORD + " FROM " + Entries.UserEntry.TABLE_NAME
+                + " WHERE " + Entries.UserEntry.COLUMN_NAME_USERNAME + " = ? AND "
+                + Entries.UserEntry.COLUMN_NAME_PASSWORD + " = ? ";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(QUERY, new String[] {username, password});
+
+        boolean success = cursor.moveToNext() ? true : false;
+
+        cursor.close();
+        db.close();
+
+        return success;
     }
 }
