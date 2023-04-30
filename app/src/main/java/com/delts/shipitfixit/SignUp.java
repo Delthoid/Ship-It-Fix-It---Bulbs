ca349880922d27ed9252f5c5c6a9f88e48998a2e
Delthoid <delthonvillanueva20@gmail.com>
2023-04-30 18:00:04 +0800

    Setup sign up screen

--- app/src/main/java/com/delts/shipitfixit/SignUp.java
+++ app/src/main/java/com/delts/shipitfixit/SignUp.java
@@ -2,13 +2,43 @@ package com.delts.shipitfixit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.delts.shipitfixit.databinding.ActivitySignUpBinding;
 
 public class SignUp extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        binding.resgisterButtonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userName = binding.usernameFieldSignup.getText().toString();
+                final String password = binding.passwordFieldSignup.getText().toString();
+
+                binding.usernameFieldSignup.setError(userName.isEmpty() ? "Username is required" : null);
+                binding.passwordFieldSignup.setError(password.isEmpty() ? "Password is required" : null);
+
+                //Mag login din dapat dito
+                if (!userName.isEmpty() || !password.isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
