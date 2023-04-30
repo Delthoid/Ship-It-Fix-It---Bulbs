cf67d6fb0af74ac1ec8b4d2585c755113448ba58
Jacob Del Rosario <jovenjacobdelrosario14@gmail.com>
2023-04-30 16:39:00 +0800

    fragments para kay delthon

--- app/src/main/java/com/delts/shipitfixit/MainActivity.java
+++ app/src/main/java/com/delts/shipitfixit/MainActivity.java
@@ -1,14 +1,42 @@
 package com.delts.shipitfixit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.delts.shipitfixit.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavController navController = Navigation.findNavController(this, R.id.navigationController);
+        NavigationUI.setupWithNavController(binding.navigationView, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                binding.toolbarTitleTextView.setText(navDestination.getLabel());
+            }
+        });
     }
 }
