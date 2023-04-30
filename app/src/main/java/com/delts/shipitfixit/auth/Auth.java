18cfd99b7eab81802ff212c2ab61513f37f46170
Delthoid <delthonvillanueva20@gmail.com>
2023-04-30 16:39:31 +0800

    Updates

--- app/src/main/java/com/delts/shipitfixit/auth/Auth.java
+++ app/src/main/java/com/delts/shipitfixit/auth/Auth.java
@@ -7,22 +7,27 @@ import com.delts.shipitfixit.models.User;
 
 public class Auth {
     private final Context context;
-
-    public Auth(Context context) {
-        this.context = context;
-    }
+    private final SharedPreferences sharedPref;
 
     private static final String preferenceFileKey = "auth";
 
+    public Auth(Context context) {
+        this.context = context;
+        this.sharedPref = context.getSharedPreferences(preferenceFileKey, Context.MODE_PRIVATE);
+    }
+
     //Login
     public void login(User user) {
-        SharedPreferences sharedPref = context.getSharedPreferences(preferenceFileKey, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("userId", user.getId());
+        editor.apply();
     }
 
     //Logout
     public void logout() {
-
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.remove("userId");
+        editor.apply();
     }
 
     public void register(Context context) {
@@ -30,7 +35,8 @@ public class Auth {
     }
 
     //Check if there is saved user id
-    public void checkLoggedUsers() {
-
+    public boolean checkLoggedUsers() {
+        final int userId = sharedPref.getInt("userId", 0);
+        return userId != 0;
     }
 }
