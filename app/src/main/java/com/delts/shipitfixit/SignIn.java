8e09b309249628c40cb9a82d8b42814acda69658
Delthoid <delthonvillanueva20@gmail.com>
2023-04-30 17:50:33 +0800

    Setup login screen

--- app/src/main/java/com/delts/shipitfixit/SignIn.java
+++ app/src/main/java/com/delts/shipitfixit/SignIn.java
@@ -2,15 +2,55 @@ package com.delts.shipitfixit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.delts.shipitfixit.databinding.ActivitySignInBinding;
+
 
 public class SignIn extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
 
-        getSupportActionBar().setTitle("Sign In");
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userName = binding.usernameField.getText().toString();
+                final String password = binding.passwordField.getText().toString();
+
+                if (userName.isEmpty()) {
+                    binding.usernameField.setError("Username cannot be empty");
+                }
+
+                if (password.isEmpty()) {
+                    binding.passwordField.setError("Password cannot be empty");
+                }
+
+                if (!password.isEmpty() || !userName.isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        binding.registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
